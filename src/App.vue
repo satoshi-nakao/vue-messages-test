e63f2015301ce5a5e04f5241c75a8e16799175c3
Your Name <you@example.com>
2021-02-05 07:09:25 +0900

    Vuejsの学習の記録として残します

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <h1>{{ 'Hello Vuejs!' }}</h1>
+   <h1>{{ 12 }}</h1>
+  <!-- 下記は上記と同じ意味 -->
+   <hr>
+   <h2>{{ message+'(data関数のmessageで表示)' }}</h2>
+   <h3>{{ number+'(data関数のnumberで表示)' }}</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // data() {
+  //   return{
+  //     message: 'Hello Vuejs!'
+  //   }
+  //}
+  data :function() {
+    return{
+      message: 'Hello Vuejs',
+      number: 12
+    }
+  }
+}
+</script>
+
